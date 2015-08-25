6e332d45bc5d45107fdb140f917f6763e9d08512
Rod Machen <mail@rodmachen.com>
2015-08-24 22:17:20 -0500

    Implement pseudo-classical instantiation.

--- src/dancer.js
+++ src/dancer.js
@@ -1,33 +1,20 @@
-// Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
+var makeDancer = function(top, left, timeBetweenSteps) {
+  this.$node = $('<span class="dancer"></span>');
+  this.timer = timeBetweenSteps;
+  this.step();
+  this.setPosition(top, left);
+};
 
-  var dancer = {};
+makeDancer.prototype.step = function() {
+  // for this.step.bind(this) the bind(this) allows the first this to be used
+  // and then, this.step just refers to the function definition of makeDancer.prototype.step
+  setTimeout(this.step.bind(this), this.timer);
+};
 
-  // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+makeDancer.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
   };
-  dancer.step();
-
-  dancer.setPosition = function(top, left){
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+  this.$node.css(styleSettings);
 };
